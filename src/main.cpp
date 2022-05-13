c6a5d24b4c98d6451f6e0f2ed144f45dda9f451b
Bartek O <bartek@fos.nl>
2022-05-13 21:25:38 +0200

    Added logic to reply with an "Hello World!" html page
    with the `LED_BUILTIN` turning on while a client is connected
    
    with custom
    Server: `Arduino Ethernet Shield` and
    X-Powered-By: `cpp`
    headers :)

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@ IPAddress ip(IP_ADDRESS);
 EthernetServer server(80);
 
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
     // sketch can hang with an SD card, solution:
     pinMode(4, OUTPUT);
     digitalWrite(4, HIGH);
@@ -48,8 +51,53 @@ void loop() {
     // if there is no client, do nothing
     if (!client) return;
 
+    digitalWrite(LED_BUILTIN, HIGH);
     Serial.println(F("There is a client!"));
 
     Serial.print(F("with ip: "));
     Serial.println(client.remoteIP());
+
+    bool currentLineIsBlank = true;
+    while (client.connected()) {
+        if (!client.available()) continue; // skip this iteration
+
+        char c = client.read(); // read the current character
+        Serial.write(c);
+
+        // if we've gotten to the end of the line (received a newline character)
+        // and the line is blank, the http request has ended (so we can send a reply)
+        if (c == '\n' && currentLineIsBlank) {
+            // respond to the client
+            client.println(F("HTTP/1.1 200 OK"));
+            client.println(F("Server: Arduino Ethernet Shield"));
+            client.println(F("X-Powered-By: cpp"));
+            client.println(F("Content-Type: text/html"));
+            client.println(F("Connection: close")); // close the connection after completing the request
+
+            client.println();
+
+            client.println(F("<!DOCTYPE HTML>"));
+            client.println(F("<html>"));
+            client.println(F("<body>"));
+            client.println(F("<h1>Hello World!</h1>"));
+            client.println(F("</body>"));
+            client.println(F("</html>"));
+            break;
+        }
+
+
+        // if we've gotten to the end of the line, mark it as blank
+        if (c == '\n') currentLineIsBlank = true;
+
+        // if we've gotten to a character, mark the line as no longer blank (\r is not not blank)
+        else if (c != '\r') currentLineIsBlank = false;
+    }
+
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection
+    client.stop();
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println(F("Client disconnected :("));
 }
