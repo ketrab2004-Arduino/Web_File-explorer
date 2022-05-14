6451bb1f8b1494c0c9f906a55b821f7d1e81921c
Bartek O <bartek@fos.nl>
2022-05-14 23:20:38 +0200

    Replaced serial prints with info macros in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,10 @@ void setup() {
     Serial.begin(9200);
     while (!Serial) ; // wait for serial port to connect. Needed for native USB port only
 
-    Serial.println(F("Starting..."));
+    infoLnF("Starting...");
 
     if (!SD.begin(SD_CS)) {
-        Serial.println(F("SD card initialization failed!"));
+        infoLnF("SD card initialization failed!");
         while (true) delay(10); // stop (no sd card to load data from)
     }
 
@@ -31,21 +31,21 @@ void setup() {
 
     // check if failed because of no hardware
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-        Serial.println(F("Ethernet shield was not found..."));
+        infoLnF("Ethernet shield was not found...");
         while (true) delay(10); // stop (no ethernet shield to use)
     }
 
     // check if failed because it isn't connected
     if (Ethernet.linkStatus() == LinkOFF) {
-        Serial.println(F("Ethernet cable is not connected..."));
+        infoLnF("Ethernet cable is not connected...");
         while (true) delay(10); // stop (not connected to anything)
     }
 
-    Serial.println(F("Beginning server..."));
+    infoLnF("Beginning server...");
     server.begin();
 
-    Serial.print(F("Ip address: "));
-    Serial.println(Ethernet.localIP());
+    infoF("Ip address: ");
+    infoLn(Ethernet.localIP());
 }
 
 void loop() {
